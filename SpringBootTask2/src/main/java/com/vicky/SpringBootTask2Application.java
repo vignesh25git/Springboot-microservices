f2eff45ea1866b386e32d2afa0d65b299ea8e3e4
Vicky <vigneshvelusamy@Vigneshs-MacBook-Pro.local>
2022-12-02 08:54:03 +0530

    tomcat dep

--- SpringBootTask2/src/main/java/com/vicky/SpringBootTask2Application.java
+++ SpringBootTask2/src/main/java/com/vicky/SpringBootTask2Application.java
@@ -2,9 +2,16 @@ package com.vicky;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootTask2Application {
+public class SpringBootTask2Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBootTask2Application.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootTask2Application.class, args);
